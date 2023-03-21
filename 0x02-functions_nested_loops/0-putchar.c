775311222beb11195215430894fa4035bb810721
bozize <bozize@gmail.com>
2023-03-21 11:21:02 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,12 @@
 #include "main.h"
-
 /**
- * main - Write a program that prints _putchar, followed by a new line
- *
- * Return: Always 0 (Success)
+ * main - main function
+ * _putchar
+ * Return: 0
  */
 
 int main(void)
-
-{
-
-	_putchar('_');
+{	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -18,7 +14,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('\n');
+return (0);
 }
-
