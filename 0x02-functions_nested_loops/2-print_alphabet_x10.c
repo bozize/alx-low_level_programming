d75606a06c897c26d20e5e51058716073f52184c
bozize <bozize@gmail.com>
2023-03-21 12:00:24 -0700

    task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 1 ; i <= 10 ;)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+			_putchar(ch);
+	}
+		_putchar('\n');
+}
