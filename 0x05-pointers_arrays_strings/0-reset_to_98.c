fc0f44b353c7cd2de3b9d81dd384ea0ba4944d63
bozize <bozize@gmail.com>
2023-03-27 06:57:50 -0700

    task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer to a int prameter
+ * and updates the value to to 98
+ * @n: pointer
+ *
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
