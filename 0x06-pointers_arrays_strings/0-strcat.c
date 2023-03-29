8a95e5f7ce17e6ae0ae70b86a246648c2b3a7036
bozize <bozize@gmail.com>
2023-03-29 12:31:59 -0700

    task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate 2 strings
+ *
+ * @dest: parameter for destination
+ * @src: parameter for source
+ *
+ * Return: destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int destlen = 0;
+	int srclen = 0;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; i <= srclen ; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
