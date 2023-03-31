a2be330f9f150f85e7649ff4e91f1079fb5d4891
bozize <bozize@gmail.com>
2023-03-31 01:43:27 -0700

    task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - function that concatenates two strings with condition
+ * @src:source string
+ * @dest:destination string
+ *
+ * @n:condition
+ *
+ * Return:dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1;
+	int len2;
+	int i;
+
+	len1 = strlen(dest);
+	len2 = strlen(src);
+
+	for (i = 0; i <= n; i++)
+
+		dest[len1 + i] = src[i];
+
+	return (dest);
+}
+
+
+
