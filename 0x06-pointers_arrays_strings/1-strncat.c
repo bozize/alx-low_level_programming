aa42b9cb4fc4e520b73f2ea457558a199a1fd16d
bozize <bozize@gmail.com>
2023-03-31 04:35:05 -0700

    task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,9 +19,11 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; src[i] != '\0'; i++)
 		srclen++;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 
 		dest[destlen + i] = src[i];
+
+	dest[destlen + i] = '\0';
 
 	return (dest);
 }
