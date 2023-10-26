90be3b056173c601bdda70713c20bd4414d39332
bozize <abdiazizibrahim56@.com>
2023-10-26 11:57:39 -0700

    power of

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - calculates power of a number
+ *
+ * @x: parameter
+ * @y: parameter
+ *
+ * Return: always 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+		else
+		{
+			return (x * pow(x, y - 1));
+		}
+}
