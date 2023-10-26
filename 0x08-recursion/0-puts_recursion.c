1a8827361e58aadf51291e745f6f1d4ef59b32b6
bozize <abdiazizibrahim56@.com>
2023-10-26 07:44:06 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,9 +11,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-	_putchar('\n');
+	i_putchar('\n');
 	return;
 	}
 }
 _putchar(*s);
-_puts_recursion(s = 1);
+_puts_recursion(s + 1);
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,8 +9,12 @@
  */
 int _strlen_recursion(char *s)
 {
+	int n = 0;
+
 	if (*s != '\0')
 	{
-		(return 1) + _strlen_recursion(s + 1);
+		n = 1 + _strlen_recursion(s + 1);
 	}
+
+	return (n);
 }
