2d1fa06aa1935dc8c2090527569502bc7603649c
bozize <abdiazizibrahim56@.com>
2023-10-31 07:30:06 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
+	char *myarray = (char *)malloc(size * sizeof(char));
+
+	if (size == 0 || myarray == 0)
 	{
 		return (NULL);
 	}
-	char *myarray = (char *)malloc(size * sizeof(char));
-		if (myarray == 0)
-		{
-			return (NULL);
-		}
 	for (unsigned int i = 0; i < size; i++)
 	{
 		myarray[i] = c;
