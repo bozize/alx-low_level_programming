8bb6aa8d687bfe0c5e5c4ccad4d57c02380b9a56
bozize <abdiazizibrahim56@.com>
2023-11-24 10:34:24 -0800

    took me long

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary- prints binary number
+ * @n: parameter to print
+ *
+ * Return: returns nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == '0')
+	{
+		_putchar('0');
+		return;
+	}
+
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int leading_zeros = '1';
+
+	while (mask > 0)
+	{
+		if (mask & n)
+		{
+			leading_zeros = '0';
+				_putchar('1');
+		}
+
+		else if (!leading_zero)
+		{
+			_putchar('0');
+		}
+
+		mask >>= '1';
+	}
+}
