d3032791183f3ed54cc5539bcb50f8b56cf25e87
bozize <abdiazizibrahim56@.com>
2023-11-24 06:18:56 -0800

    binary heh

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint- converts binary to interger
+ * @*b:the binary number
+ *
+ * Return:unsigned interger
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int final;
+	int hold;
+
+	if(b == NULL)
+	{
+		return (0);
+	}
+
+	unsigned int final = 0;
+	int hold = 0;
+
+	while(b[hold] != '\0')
+	{
+		if(b[hold] != '0' && b[hold] != '1')
+		{
+			return (0);
+		}
+
+		final= final * 2 + (b[hold] - '0');
+		hold ++;
+	}
+
+	return (final);
+}
